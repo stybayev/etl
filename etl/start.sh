736a5eb307bc2da38c5609508875128a579dc24a
dosxana@gmail.com <dosxana@gmail.com>
2024-02-09 21:02:41 +0600

    Спринт 4/15: 3: ETL → Тема 2/3: - реализовал запуск приложения в Docker

--- etl/start.sh
+++ etl/start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-set -euo pipefail
+set -e
 
+# Ожидание доступности Postgres
+echo "Waiting for Postgres to be ready..."
+while ! nc -z db 5432; do
+  sleep 0.1
+done
 
-echo "Running ETL process..."
-python main.py
+echo "Postgres is ready!"
 
-
+# Запуск основного ETL процесса
+exec python main.py
